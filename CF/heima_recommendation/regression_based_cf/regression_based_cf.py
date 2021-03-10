4488e61c0fbd735f89b0af5114437d18d0a37c5c
jiangpeng.jiang <jiangpeng.jiang@zhaopin.com.cn>
2021-03-10 14:54:12 +0800

    update cf

--- CF/heima_recommendation/regression_based_cf/regression_based_cf.py
+++ CF/heima_recommendation/regression_based_cf/regression_based_cf.py
@@ -28,7 +28,100 @@ pred_b_{ui} = mu + b_{u} + b_{i}
 """
 
 
+def data_split(data_path, x=0.8, random=False):
+    '''
+    切分数据集， 这里为了保证用户数量保持不变，将每个用户的评分数据按比例进行拆分
+    :param data_path: 数据集路径
+    :param x: 训练集的比例，如x=0.8，则0.2是测试集
+    :param random: 是否随机切分，默认False
+    :return: 用户-物品评分矩阵
+    '''
+    print("开始切分数据集...")
+    # 设置要加载的数据字段的类型
+    dtype = {"userId": np.int32, "movieId": np.int32, "rating": np.float32}
+    # 加载数据，我们只用前三列数据，分别是用户ID，电影ID，已经用户对电影的对应评分
+    ratings = pd.read_csv(data_path, dtype=dtype, usecols=range(3))
+
+    testset_index = []
+    # 为了保证每个用户在测试集和训练集都有数据，因此按userId聚合
+    for uid in ratings.groupby("userId").any().index:
+        user_rating_data = ratings.where(ratings["userId"]==uid).dropna()
+        if random:
+            # 因为不可变类型不能被 shuffle方法作用，所以需要强行转换为列表
+            index = list(user_rating_data.index)
+            np.random.shuffle(index)    # 打乱列表
+            _index = round(len(user_rating_data) * x)
+            testset_index += list(index[_index:])
+        else:
+            # 将每个用户的x比例的数据作为训练集，剩余的作为测试集
+            index = round(len(user_rating_data) * x)
+            testset_index += list(user_rating_data.index.values[index:])
+
+    testset = ratings.loc[testset_index]
+    trainset = ratings.drop(testset_index)
+    print("完成数据集切分...")
+    return trainset, testset
+
+
+def accuray(predict_results, method="all"):
+    '''
+    准确性指标计算方法
+    :param predict_results: 预测结果，类型为容器，每个元素是一个包含uid,iid,real_rating,pred_rating的序列
+    :param method: 指标方法，类型为字符串，rmse或mae，否则返回两者rmse和mae
+    :return:
+    '''
+
+    def rmse(predict_results):
+        '''
+        rmse评估指标
+        :param predict_results:
+        :return: rmse
+        '''
+        length = 0
+        _rmse_sum = 0
+        for uid, iid, real_rating, pred_rating in predict_results:
+            length += 1
+            _rmse_sum += (pred_rating - real_rating) ** 2
+        return round(np.sqrt(_rmse_sum / length), 4)
+
+    def mae(predict_results):
+        '''
+        mae评估指标
+        :param predict_results:
+        :return: mae
+        '''
+        length = 0
+        _mae_sum = 0
+        for uid, iid, real_rating, pred_rating in predict_results:
+            length += 1
+            _mae_sum += abs(pred_rating - real_rating)
+        return round(_mae_sum / length, 4)
+
+    def rmse_mae(predict_results):
+        '''
+        rmse和mae评估指标
+        :param predict_results:
+        :return: rmse, mae
+        '''
+        length = 0
+        _rmse_sum = 0
+        _mae_sum = 0
+        for uid, iid, real_rating, pred_rating in predict_results:
+            length += 1
+            _rmse_sum += (pred_rating - real_rating) ** 2
+            _mae_sum += abs(pred_rating - real_rating)
+        return round(np.sqrt(_rmse_sum / length), 4), round(_mae_sum / length, 4)
+
+    if method.lower() == "rmse":
+        rmse(predict_results)
+    elif method.lower() == "mae":
+        mae(predict_results)
+    else:
+        return rmse_mae(predict_results)
+
+
 class BaselineCFBySGD(object):
+
     def __init__(self, number_epochs, alpha, reg, columns=["uid", "iid", "rating"]):
         # 梯度下降最高迭代次数
         self.number_epochs = number_epochs
@@ -74,18 +167,39 @@ class BaselineCFBySGD(object):
         return bu, bi
 
     def predict(self, uid, iid):
+        '''评分预测'''
+        if iid not in self.items_ratings.index:
+            raise Exception("无法预测用户<{uid}>对电影<{iid}>的评分，因为训练集中缺失<{iid}>的数据".format(uid=uid, iid=iid))
+
         predict_rating = self.global_mean + self.bu[uid] + self.bi[iid]
         return predict_rating
 
+    def test(self,testset):
+        '''预测测试集数据'''
+        for uid, iid, real_rating in testset.itertuples(index=False):
+            try:
+                pred_rating = self.predict(uid, iid)
+            except Exception as e:
+                print(e)
+            else:
+                yield uid, iid, real_rating, pred_rating
+
 
 if __name__ == '__main__':
-    dtype = [("userId", np.int32), ("movieId", np.int32), ("rating", np.float32)]
-    dataset = pd.read_csv("../data/ml-latest-small/ratings.csv", usecols=range(3), dtype=dict(dtype))
-
+    trainset, testset = data_split("datasets/ml-latest-small/ratings.csv", random=True)
     bcf = BaselineCFBySGD(20, 0.1, 0.1, ["userId", "movieId", "rating"])
-    bcf.fit(dataset)
+    bcf.fit(trainset)
+    pred_results = bcf.test(testset)
+    rmse, mae = accuray(pred_results)
+    print("rmse: ", rmse, "mae: ", mae)
 
-    while True:
-        uid = int(input("uid: "))
-        iid = int(input("iid: "))
-        print(bcf.predict(uid, iid))
+
+# if __name__ == '__main__':
+#     dtype = [("userId", np.int32), ("movieId", np.int32), ("rating", np.float32)]
+#     dataset = pd.read_csv("../data/ml-latest-small/ratings.csv", usecols=range(3), dtype=dict(dtype))
+#     bcf = BaselineCFBySGD(20, 0.1, 0.1, ["userId", "movieId", "rating"])
+#     bcf.fit(dataset)
+#     while True:
+#         uid = int(input("uid: "))
+#         iid = int(input("iid: "))
+#         print(bcf.predict(uid, iid))
